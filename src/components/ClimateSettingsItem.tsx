775c8dfad96d5b2cc3b6114226e5f1bf0f6b6c2a
Tom Santens <tom.santens@gmail.com>
2024-09-07 17:05:07 +0200

    Import climate settings legacy code

---
+++ src/components/ClimateSettingsItem.tsx
@@ -0,0 +1,227 @@
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudRain, faSmog, faSun, faThermometerHalf } from '@fortawesome/free-solid-svg-icons';
+import {Slider, FormGroup, FormControlLabel, Switch } from '@mui/material';
+import { LocalizationProvider, TimePicker } from '@mui/lab';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+//import { TimeOfDayObject } from '../../utilities/climate';
+
+
+const ClimateSettingsItem = (props:{data:any, setSettings:( settings:any)=>void }) => {
+
+    // on initialization, preload all settings fields
+    useEffect(()=>{
+        // set the NAME
+        setName(props.data.name);
+        // TODO: initialize START TIME
+        setStartTime([props.data.startHours, props.data.startMinutes]);
+        setStartDate(setDateFromTime( props.data.startHours, props.data.startMinutes ));
+        // initialize TEMPERATURE range
+        setTemperatureRange( [props.data.temperature.minimum, props.data.temperature.maximum] );
+        // initialize LIGHTS (TODO: UV!)
+        if( props.data.lighting.maximum >= 50){
+            setDaylightLights( true );          // initialize Daylight
+            setUvLights( true );                // initialize UV
+        }
+        else {
+            setDaylightLights( false );          // initialize Daylight
+            setUvLights( false );                // initialize UV
+        }
+        // initialize HUMIDITY range
+        setHumidityRange( [props.data.humidity.minimum, props.data.humidity.maximum] );
+        // TODO initialize RAIN timings
+        setRainInterval(props.data.humidity.rainInterval);
+        setRainDuration(props.data.humidity.rainDuration);
+
+    },[]);
+
+    // on every change, update the full object
+    useEffect(()=>{
+        let minLightIntensity=0;
+        let maxLightIntensity=0;
+
+        // Convert Lighting settings
+        if( daylightLights ){
+            minLightIntensity = 55;
+            maxLightIntensity = 100;
+        }
+        else{
+            minLightIntensity = 0;
+            maxLightIntensity = 45;
+        }
+
+        // Create the new TimeOfDay Object
+        const newTimeOfDaySettings:any = {
+            uuid: props.data.uuid,                  // UUID
+            name: name,                             // name of this time of day
+            startHours: startTime[0],               // starttime hours
+            startMinutes: startTime[1],             // starttime minutes
+            endHours: 0,                     // TODO: calculate endtime hours
+            endMinutes: 0,                   // TODO: calculate endtime minutes
+            lighting: {
+                minIntensity: minLightIntensity,    // minimum light intensity in %
+                maxIntensity: maxLightIntensity     // maximum light intensity in %
+            },
+            temperature: {
+                minTemperature: (temperatureRange as number[])[0],     // minimum temperature in deg. Celcius
+                maxTemperature: (temperatureRange as number[])[1]      // maximum temperature in deg. Celcius
+            },
+            humidity: {
+                minRHumidity: (humidityRange as number[])[0],   // minimum relative humidity in %
+                maxRHumidity: (humidityRange as number[])[1],   // maximum relative humidity in %
+                rainInterval: rainInterval,         // interval in minutes
+                rainDuration: rainDuration          // duration in seconds
+            }
+        }
+        //console.log(newTimeOfDaySettings);
+        props.setSettings(newTimeOfDaySettings);
+    });
+
+    /*  
+     *  Settings components logic
+     */
+
+    // Name
+    const[ name, setName ] = useState('');
+
+    // Start Time
+    const[ startTime, setStartTime ] = useState<number[]>([]);      // HH:mm
+    const[ startDate, setStartDate ] = useState<Date>();
+
+    const changeTimeHandler = ( value:any ) =>{
+        var date = new Date();
+        date.setTime(value);
+        setStartDate(date);
+        setStartTime([date.getHours(),date.getMinutes()]);
+    }
+
+    const setDateFromTime = ( hours:number, minutes:number ) => {
+        var date = new Date();
+        date.setHours( hours, minutes, 0);   // Set hours, minutes and seconds
+        return date;
+    }
+
+    // Rain interval & duration
+    const[ rainInterval, setRainInterval ] = useState<number>(0);
+    const[ rainDuration, setRainDuration ] = useState<number>(0);
+
+    const handleRainIntervalChange = ( event:React.FormEvent<HTMLInputElement>) => {
+        setRainInterval(parseInt(event.currentTarget.value));
+    }
+
+    const handleRainDurationChange = ( event:React.FormEvent<HTMLInputElement>) => {
+        setRainDuration(parseInt(event.currentTarget.value));
+    }
+
+    // range slider Temperature
+    // range slider info: https://mui.com/api/slider-unstyled/
+    const[ temperatureRange, setTemperatureRange ] = useState<number | number[]>([]);
+
+    const handleTemperatureChange = ( event: Event, value: number | number[], activeThumb: number) => {
+        setTemperatureRange( value )
+    };
+
+    // range slider Humidity
+    const[ humidityRange, setHumidityRange ] = useState<number | number[]>([]);
+
+    const handleHumidityChange = ( event: Event, value: number | number[], activeThumb: number) => {
+        setHumidityRange( value )
+    };
+
+    // Switch Lights - Daylight
+    const[ daylightLights, setDaylightLights ] = useState<boolean>(false);
+    const handleDaylightChange = ( event:any, checked:boolean ) => {
+        setDaylightLights( checked );
+    }
+    
+    const Daylight = () =>{
+        if( daylightLights ){
+            return <>on</>;
+        }
+        else{
+            return <>off</>;
+        }
+    }
+
+    // Switch Lights - UV
+    const[ uvLights, setUvLights ] = useState<boolean>(false);
+    const handleUvlightChange = ( event:any, checked:boolean ) => {
+        setUvLights( checked );
+    }
+
+    const Uvlight = () =>{
+        if( uvLights ){
+            return <>on</>;
+        }
+        else{
+            return <>off</>;
+        }
+    }
+
+    return(
+        <>
+            <h3>{name}</h3>
+            <p>{name} starts at <strong>{startTime[0]}:{startTime[1]}</strong>and ends <strong>{props.data.endHours}:{props.data.endMinutes}</strong>.</p>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <TimePicker
+                    ampm={false}
+                    label=''
+                    value={startDate}
+                    onChange={ changeTimeHandler }
+                    renderInput={(params:any) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
+            <br /><br />
+            <h4><FontAwesomeIcon icon={faThermometerHalf} /> Temperature</h4>
+            <p>The temperature ranges between <strong>{(temperatureRange as number[])[0]}&deg;C</strong> and <strong>{(temperatureRange as number[])[1]}&deg;C</strong>.</p>
+            <Slider
+                getAriaLabel={() => 'Temperature range'}
+                value={temperatureRange}
+                onChange={handleTemperatureChange}
+                valueLabelDisplay="auto"
+                min={5}
+                max={70}
+                sx={{
+                    maxWidth: 500,
+                    color: 'var(--freya_light_green)',
+                  }}
+                />
+            <hr />
+            <h4><FontAwesomeIcon icon={faSun} /> Lights</h4>
+            <p>Daylights are turned <strong><Daylight /></strong>, and UV lights are turned <strong><Uvlight /></strong>.</p>
+            <FormGroup>
+                <FormControlLabel control={<Switch
+                    checked={daylightLights}
+                    onChange={handleDaylightChange}
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    />} label="Daylight" />
+                <FormControlLabel control={<Switch 
+                    checked={uvLights}
+                    onChange={handleUvlightChange}
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />} label="UV" />
+            </FormGroup>
+            <hr />
+            <h4><FontAwesomeIcon icon={faSmog} /> Humidity</h4>
+            <p>The humidity ranges between <strong>{(humidityRange as number[])[0]}%</strong> and <strong>{(humidityRange as number[])[1]}%</strong>.</p>
+            <Slider
+                getAriaLabel={() => 'Humidity range'}
+                value={humidityRange}
+                onChange={handleHumidityChange}
+                valueLabelDisplay="auto"
+                    sx={{
+                    maxWidth: 500,
+                    color: 'var(--freya_light_green)'
+                  }}
+            />
+            <hr />
+            <h4><FontAwesomeIcon icon={faCloudRain} /> Rain</h4>
+            <p>It will rain every <strong>{rainInterval} minutes</strong> for <strong>{rainDuration} seconds</strong>.</p>
+            <input type="number" name="rainInterval" min={0} onChange={handleRainIntervalChange} value={String(rainInterval)}></input> minutes<br/>
+            <input type="number" name="rainDuration" min={0} onChange={handleRainDurationChange} value={String(rainDuration)}></input> seconds
+        </>
+    )
+}
+
+export default ClimateSettingsItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,27 @@ import { Alert, Button, Container, Tab, Tabs } from "react-bootstrap";
 import VivariumData from "../components/VivariumData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartLine, faCogs } from "@fortawesome/free-solid-svg-icons";
+import ClimateSettingsItem from "../components/ClimateSettingsItem";
 
 const Home = ()=>{
+    const data={
+        startHours: 11,
+        startMinutes: 13,
+        temperature:{
+            minimum: 20,
+            maximum: 30
+        },
+        humidity:{
+            minimum: 45,
+            maximum: 55,
+            rainInterval: 30,
+            rainDuration: 5
+        },
+        lighting:{
+            minimum: 75,
+            maximum: 100
+        }
+    }
     return(
         <Container>
             <Tabs defaultActiveKey="data" className="mb-3">
@@ -14,6 +33,7 @@ const Home = ()=>{
                     <Alert>
                         We're still working on this!
                     </Alert>
+                    <ClimateSettingsItem data={data} setSettings={()=>{}} />
                 </Tab>
             </Tabs>
         </Container>
