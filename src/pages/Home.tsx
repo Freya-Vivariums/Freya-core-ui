6994e24f7ac56eb08e76754f12589c2e9bd7e310
Tom Santens <tom.santens@gmail.com>
2024-09-08 14:05:39 +0200

    Home: Configuration tab

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Container, Tab, Tabs } from "react-bootstrap";
 import VivariumData from "../components/VivariumData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartLine, faSliders } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine, faCogs, faSliders } from "@fortawesome/free-solid-svg-icons";
 import ClimateSettings from "../components/ClimateSettings/ClimateSettings";
 
 const Home = ()=>{
@@ -12,6 +12,9 @@ const Home = ()=>{
                     <VivariumData id="blub" vivariumSocket={{}} />
                 </Tab>
                 <Tab title={<><FontAwesomeIcon icon={faSliders}/> Settings</>} eventKey={"settings"}>
+                    <ClimateSettings />
+                </Tab>
+                <Tab title={<><FontAwesomeIcon icon={faCogs}/> Configuration</>} eventKey={"configuration"} disabled>
                     <ClimateSettings />
                 </Tab>
             </Tabs>
