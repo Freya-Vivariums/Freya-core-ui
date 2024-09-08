5ce4df7d4abaf9a0dc6833e9a418c32b32b98c88
Tom Santens <tom.santens@gmail.com>
2024-09-08 12:40:53 +0200

    Styling improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import VivariumData from "../components/VivariumData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartLine, faCogs } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine, faCogs, faSliders } from "@fortawesome/free-solid-svg-icons";
 import ClimateSettingsItem from "../components/ClimateSettingsItem";
 
 const Home = ()=>{
@@ -30,7 +30,7 @@ const Home = ()=>{
                 <Tab title={<><FontAwesomeIcon icon={faChartLine}/> Monitor</>} eventKey={"monitor"}>
                     <VivariumData id="blub" vivariumSocket={{}} />
                 </Tab>
-                <Tab title={<><FontAwesomeIcon icon={faCogs}/> Settings</>} eventKey={"settings"}>
+                <Tab title={<><FontAwesomeIcon icon={faSliders}/> Settings</>} eventKey={"settings"}>
                     <Row>
                         <Col>
                             <ClimateSettingsItem data={data} setSettings={()=>{}} />
@@ -40,7 +40,7 @@ const Home = ()=>{
                         </Col>
                     </Row>
                     <hr/>
-                    <Button variant="danger" className="mb-3">Reset</Button>&nbsp;
+                    <Button variant="secondary" className="mb-3">Reset</Button>&nbsp;
                     <Button variant="primary" className="mb-3">Save</Button>
                 </Tab>
             </Tabs>
