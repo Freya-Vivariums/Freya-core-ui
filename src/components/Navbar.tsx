a1141a2b9256a3b955a551a34d2605f1f3ea10d7
Tom Santens <tom.santens@gmail.com>
2024-09-08 13:13:54 +0200

    Navbar: logout button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
-import { Container, Navbar } from "react-bootstrap";
+import { faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 const NavigationBar = ()=>{
 
     return(
-        <Navbar sticky="top" bg={'dark'} data-bs-theme={'dark'}>
+        <Navbar sticky="top" bg={'dark'} variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand href='/'>
                     <img src={'/Freya-logo.png'} alt="Freya logo" height={'32px'}/>&nbsp;
                 </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav.Link onClick={()=>{}} style={{ cursor: 'pointer', color:'var(--freya_light_grey)' }} className="ms-auto">
+                        <FontAwesomeIcon icon={faSignOut} />
+                    </Nav.Link>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
